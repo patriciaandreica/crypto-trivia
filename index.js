9ecc6c07027186404fdbd9cba5ed798e676bb112
Patricia Andreica <peandreica1116@eagle.fgcu.edu>
2022-11-20 21:43:29 -0500

    added question

---
+++ index.js
@@ -0,0 +1,10 @@
+const quizData = [
+    {
+        question: "Aprox. how many Bitcoins are left to be mined?",
+        a: "2 million",
+        b: "None",
+        c: "50",
+        d: "Bitcoin has unlimited supply",
+        correct: "a"
+    }
+]
